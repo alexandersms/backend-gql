9ce42df0905d0e23920637e5f5f34553d5252195
Alex Simisi Nta <smsxander@gmail.com>
2021-12-28 04:00:44 +0100

    inport cors

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const colors = require('colors')
+const cors = require('cors')
 const app = express()
 const PORT = 5000
 
+app.use(cors())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Express !!')
 })
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`Server up and running on http://localhost:${PORT}`.yellow.bold)
 })
